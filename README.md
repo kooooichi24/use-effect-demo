50b18027329c3846954e2e7925978a8db887f838
kooooichi24 <ex.kouichi1224@gmail.com>
2022-08-20 19:09:07 +0900

    [init]: first commit template next project

---
+++ README.md
@@ -0,0 +1,8 @@
+# use-effect-demo
+## Installation
+```sh
+$ git clone https://github.com/kooooichi24/use-effect-demo.git
+$ cd use-effect-demo
+$ npm install
+$ npm run dev:mock
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { IndexTemplate } from '../components/templates'
+
+const Home: NextPage = () => {
+  return <IndexTemplate />
+}
+
+export default Home
