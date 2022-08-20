0d88b9f86c4c7fec841c18be2666e229244f11a5
kooooichi24 <ex.kouichi1224@gmail.com>
2022-08-20 23:10:12 +0900

    [feat]: fetch github repository starts

--- src/components/FetchComponent.tsx
+++ src/components/FetchComponent.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export const FetchComponent = () => {
   const [data, setData] = useState({
@@ -7,6 +8,13 @@ export const FetchComponent = () => {
   })
 
   // ここでuseEffectを使ってstar数を取得してみましょう!
+  useEffect(() => {
+    console.log('called useEffect callback')
+    ;(async () => {
+      const response = await axios.get('https://api.github.com/repos/facebook/react')
+      setData({ subscribers: response.data.subscribers_count, stars: response.data.stargazers_count })
+    })()
+  }, [])
 
   return (
     <div>
