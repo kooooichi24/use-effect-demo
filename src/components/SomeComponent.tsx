74db075d803e9701ae2df4dd56486976169041ed
kooooichi24 <ex.kouichi1224@gmail.com>
2022-08-20 22:53:08 +0900

    fix

--- src/components/SomeComponent.tsx
+++ src/components/SomeComponent.tsx
@@ -8,7 +8,7 @@ export const SomeComponent = ({ someFlag }: SomeComponentProps) => {
   const count = useRef(0)
   useEffect(() => {
     someFlag && count.current++
-  }, [someFlag])
+  })
 
   return <p>rendering count is {count.current}</p>
 }
